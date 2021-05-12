7f393d58425ec8d9ea2a70a8ab99799934e9b73e
JustinMolema420 <justmol0608@gmail.com>
2021-05-12 15:17:55 +0200

    changed the appearance of the navbar, added icons

--- Findr/src/app/navbar/navbar.component.ts
+++ Findr/src/app/navbar/navbar.component.ts
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  // activeButton:any = "games";
+
   constructor() { }
 
   ngOnInit(): void {
     
   }
+
+  // getButton():any{
+  //   return this.activeButton;
+  // }
+
+  // setButton(newButton:any){
+  //   this.activeButton = newButton;
+  // }
 
 }
