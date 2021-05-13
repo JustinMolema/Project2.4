456a2c5a1ab7d8e982cc7088ef3e1d858b9f3814
Anne Pier Merkus <g.a.p.merkus@st.hanze.nl>
2021-05-13 03:28:46 +0200

    merges and changed button borders

--- Findr/src/app/app.module.ts
+++ Findr/src/app/app.module.ts
@@ -1,5 +1,5 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { LoginModule } from './User/login/login.module';
 import { SignupModule } from './User/signup/signup.module';
 import { PasswordforgottenModule } from './User/passwordforgotten/passwordforgotten.module';
 import { GamemenuModule } from './User/gamemenu/gamemenu.module';
-import { NavbarModule } from './navbar/navbar.module';
+import { NavbarModule } from './User/navbar/navbar.module';
 import { Routes, RouterModule } from '@angular/router';
 import { TopbarModule } from './User/topbar/topbar.module';
 import { ChatmenuModule } from './User/chatmenu/chatmenu.module';
@@ -22,30 +22,30 @@ import { ReporteduserModule } from './admin/reporteduser/reporteduser.module';
 
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000',
-    }),
-    LoginModule,
-    SignupModule,
-    PasswordforgottenModule,
-    FriendsmenuModule,
-    GamemenuModule,
-    NavbarModule,
-    TopbarModule,
-    ChatmenuModule,
-    AdminLoginModule,
-    SupportticketModule,
-    ReporteduserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the app is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+        LoginModule,
+        SignupModule,
+        PasswordforgottenModule,
+        FriendsmenuModule,
+        GamemenuModule,
+        NavbarModule,
+        TopbarModule,
+        ChatmenuModule,
+        AdminLoginModule,
+        SupportticketModule,
+        ReporteduserModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
 export class AppModule {
 }
