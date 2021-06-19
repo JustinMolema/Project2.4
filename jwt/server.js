5f29826c42474a2b62b063138b2d17b00e2ebd27
robbinkok01@gmail.com <robbinkok01@gmail.com>
2021-06-19 19:42:12 +0200

    Email now displays @ instead of %40
    Co-authored-by: AnnePierMerkus <AnnePierMerkus@users.noreply.github.com>

--- jwt/server.js
+++ jwt/server.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 const test = require('./api.js');
 const http = require('http');
 const {Console} = require('console');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb'}));
@@ -615,14 +615,13 @@ app.post('/api/user/login', (req, res) => {
 })
 
 // api call to create user in database
-app.post('/api/user', async (req, res) => {
+app.post('/api/user/signup', async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
 
     // encode so that special symbols dont destroy DB
     const username = encodeURIComponent(req.body.username);
     const password = encodeURIComponent(req.body.password);
     const email = encodeURIComponent(req.body.email);
-
     const saltRounds = 10;
     bcrypt.genSalt(saltRounds, function (err, salt) {
         bcrypt.hash(password, salt, function (err, hash) {
