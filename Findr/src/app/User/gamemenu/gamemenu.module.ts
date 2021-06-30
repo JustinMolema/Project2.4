893de272530198ca703e54ca97113a62a1d4d3f8
Merel Foekens <mwfoekens@gmail.com>
2021-06-30 16:00:41 +0200

    Profile refresh fixed

--- Findr/src/app/User/gamemenu/gamemenu.module.ts
+++ Findr/src/app/User/gamemenu/gamemenu.module.ts
@@ -4,6 +4,7 @@ import {GamemenuComponent} from './gamemenu.component';
 import {GameComponent} from './game/game.component';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatButtonModule} from '@angular/material/button';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -13,6 +14,7 @@ import {MatButtonModule} from '@angular/material/button';
     imports: [
         CommonModule,
         MatTooltipModule,
+        ReactiveFormsModule,
     ], exports: [
         GamemenuComponent,
         GameComponent,
